381b3605db9c51b7bbdade7e92c473f846317f4d
Jacobo <jacobo@fixel.co.jp>
2022-08-25 17:57:22 +0900

    routes setup and error handler

--- backend/routes/goalRoutes.ts
+++ backend/routes/goalRoutes.ts
@@ -1,21 +1,38 @@
 import express from "express";
+import asyncHandler from "express-async-handler";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get goals" });
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Get goals" });
+  })
+);
 
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "Set goal" });
-});
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    if (!req.body.text) {
+      res.status(400);
+      throw new Error("Please add a text field");
+    }
+    res.status(200).json({ message: "Set goal" });
+  })
+);
 
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
-});
+router.put(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Update goal ${req.params.id}` });
+  })
+);
 
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
-});
+router.delete(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Delete goal ${req.params.id}` });
+  })
+);
 
 module.exports = router;
--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import { errorHandler } from "./middleware/errorMiddleware";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(errorHandler);
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
