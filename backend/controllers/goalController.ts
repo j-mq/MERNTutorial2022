feebe5498f2ece4a7ca65e3818c7f81c4c6a143f
Jacobo <jacobo@fixel.co.jp>
2022-08-28 18:04:19 +0900

    Authentication and Authorization done

--- backend/controllers/goalController.ts
+++ backend/controllers/goalController.ts
@@ -1,12 +1,15 @@
 import asyncHandler from "express-async-handler";
 import { Goal } from "../models/goalModel";
+import { User } from "../models/userModel";
 
 //@desc   Get goals
 //@route  GET /api/goals
 //@access Private
 export const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
-  res.status(200).json(goals);
+  if (req.user) {
+    const goals = await Goal.find({ user: req.user.id });
+    res.status(200).json(goals);
+  }
 });
 
 //@desc   Set goals
@@ -17,8 +20,10 @@ export const setGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please provide a text value");
   }
-  const goal = await Goal.create({ text: req.body.text });
-  res.status(200).json(goal);
+  if (req.user) {
+    const goal = await Goal.create({ text: req.body.text, user: req.user.id });
+    res.status(200).json(goal);
+  }
 });
 
 //@desc   Update goal
@@ -30,10 +35,26 @@ export const updateGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Goal not found");
   }
-  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  res.status(200).json(updatedGoal);
+
+  if (req.user) {
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      res.status(401);
+      throw new Error("User not found");
+    }
+
+    //Make sure the loged in user matches the goal user
+    if (goal.user.toString() !== user.id) {
+      res.status(401);
+      throw new Error("User not authorized");
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedGoal);
+  }
 });
 
 //@desc   Delete goal
@@ -45,6 +66,21 @@ export const deleteGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Goal not found");
   }
-  await goal.remove();
-  res.status(200).json({ id: req.params.id });
+
+  if (req.user) {
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      res.status(401);
+      throw new Error("User not found");
+    }
+
+    //Make sure the loged in user matches the goal user
+    if (goal.user.toString() !== user.id) {
+      res.status(401);
+      throw new Error("User not authorized");
+    }
+    await goal.remove();
+    res.status(200).json({ id: req.params.id });
+  }
 });
--- backend/routes/goalRoutes.ts
+++ backend/routes/goalRoutes.ts
@@ -6,9 +6,11 @@ import {
   deleteGoal,
 } from "../controllers/goalController";
 
+import { protect } from "../middleware/authMiddleware";
+
 const router = express.Router();
 
-router.route("/").get(getGoals).post(setGoal);
-router.route("/:id").delete(deleteGoal).put(updateGoal);
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").delete(protect, deleteGoal).put(protect, updateGoal);
 
 module.exports = router;
